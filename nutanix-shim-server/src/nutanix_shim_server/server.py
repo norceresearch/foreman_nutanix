04dd6a22b26a8f404b81fc86952b608a0dbd2c34
Miles Granger <mgra@norceresearch.no>
2025-11-14 09:05:21 +0100

    Initial list-networks endpoint impl

--- nutanix-shim-server/src/nutanix_shim_server/server.py
+++ nutanix-shim-server/src/nutanix_shim_server/server.py
@@ -6,15 +6,19 @@ from nutanix_shim_server.clustermgmt import ClusterMgmt
 from nutanix_shim_server.routes.clustermgmt import router as clustermgmt_router
 from nutanix_shim_server.vmm import VirtualMachineMgmt
 from nutanix_shim_server.routes.vmm import router as vmm_router
+from nutanix_shim_server.networking import Networking
+from nutanix_shim_server.routes.networking import router as networking_router
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     app.state.clustermgmt = ClusterMgmt()
     app.state.vmm = VirtualMachineMgmt()
+    app.state.networking = Networking()
     yield
 
 
 app = FastAPI(lifespan=lifespan)
 app.include_router(clustermgmt_router)
 app.include_router(vmm_router)
+app.include_router(networking_router)
