5e54e1caf73a3242e5a644da18e18a08843d2452
Miles Granger <mgra@norceresearch.no>
2025-12-03 09:14:25 +0100

    Shim server support serving w/ uvicorn https

--- nutanix-shim-server/src/nutanix_shim_server/__init__.py
+++ nutanix-shim-server/src/nutanix_shim_server/__init__.py
@@ -1,16 +1,39 @@
 import sys
+import subprocess
 
 from fastapi_cli.cli import main as fastapi_main
 
 
 def main() -> None:
     """Entry point that wraps fastapi-cli with the app path pre-configured."""
-    # Insert 'run' command and app path, then append any user-provided args
-    sys.argv = [
-        "fastapi",
-        "run",
-        "--entrypoint",
-        "nutanix_shim_server.server:app",
-        *sys.argv[1:],
-    ]
-    fastapi_main()
+    if "--help" in sys.argv:
+        msg = """
+Useage: `nutanix-shim-server` to run server on http using port 8000, all options are passed to `fastapi run`.
+
+        When used with `--ssl-certfile' and `--ssl-keyfile`, it will run with `uvicon` on https, all options will then be passed to `uvicorn`.
+        """
+        print(msg)
+        return
+
+    # If user supplied certificates, when we run with uvicorn
+    if "--ssl-certfile" in sys.argv and "--ssl-keyfile" in sys.argv:
+        cmd = [
+            "uvicorn",
+            "nutanix_shim_server.server:app",
+            *sys.argv[1:],
+        ]
+        r = -1
+        try:
+            r = subprocess.check_call(cmd)
+        except KeyboardInterrupt:
+            pass
+        sys.exit(r)
+    else:
+        sys.argv = [
+            "fastapi",
+            "run",
+            "--entrypoint",
+            "nutanix_shim_server.server:app",
+            *sys.argv[1:],
+        ]
+        fastapi_main()
