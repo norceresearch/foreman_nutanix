8ee1985b079f1a0b3763c42a8d49a0fc53ae1611
Miles Granger <mgra@norceresearch.no>
2026-01-08 12:33:35 +0100

    Add timeout config settings

--- nutanix-shim-server/src/nutanix_shim_server/networking.py
+++ nutanix-shim-server/src/nutanix_shim_server/networking.py
@@ -23,6 +23,8 @@ class Networking:
         self.config.port = ctx.nutanix_host_port
         self.config.client_certificate_file = ctx.nutanix_client_certificate_file
         self.config.root_ca_certificate_file = ctx.nutanix_root_ca_certificate_file
+        self.config.connect_timeout = ctx.nutanix_connect_timeout_secs * 1000
+        self.config.read_timeout = ctx.nutanix_read_timeout_secs * 1000
 
     @property
     def client(self) -> net.ApiClient:
